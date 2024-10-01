80b5abd5787d42ae0d9a1751ecf14d8f1ca20483
Shubhkarman SIngh <shubhkarmansingh3105@gmail.com>
2024-10-01 22:28:35 +0530

    Update gazebo_launch.py

--- src/runs/launch/gazebo_launch.py
+++ src/runs/launch/gazebo_launch.py
@@ -1,1 +1,16 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+
+def generate_launch_description():
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'],
+            output='screen'),
+        Node(
+            package='runs',
+            executable='spawn_entity.py',
+            arguments=['-entity', 'my_robot', '-file', '$(find runs)/urdf/robot.urdf'],
+            output='screen')
+    ])
 
